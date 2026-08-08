3afb1ff93b70b2331471890a0f910b799ccd0e44
Jung Jinyoung <ya_1001@naver.com>
2026-08-08 15:34:53 +0900

    [Passed] 달에서 무게 구하기 (110ms, 9MB)

---
+++ trail0/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int w = 13;
+        double g = 0.165;
+        System.out.printf("%d * %.6f = %.6f", w, g, w * g);    
+    }
+}
