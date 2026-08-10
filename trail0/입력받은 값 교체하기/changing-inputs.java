95f84121a34f478ee5a26773422fe9b210bb313d
Jung Jinyoung <ya_1001@naver.com>
2026-08-10 10:36:51 +0900

    [Passed] 입력받은 값 교체하기 (123ms, 10MB)

---
+++ trail0/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        String a = input[0];
+        String b = input[1];
+        System.out.println(b+ " "+a);
+
+    }
+}
